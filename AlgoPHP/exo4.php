6e398d3a9da6a9653dea7a3d14c09229a8122d38
Hafida3412 <lounsy81@gmail.com>
2024-02-21 22:09:33 +0100

    EXERCICE 6

--- AlgoPHP/exo4.php
+++ AlgoPHP/exo4.php
@@ -1,15 +1,15 @@
 <h1>Exercice 4</h1>
 
-<p>Ecrire un algorithme permettant de savoir si une phrase est 
+<p>Ecrire un algorithme permettant de savoir si une phrase est un 
     palindrome.</P>
 
 
 <?php
+$phrase1 = "Engage le jeu que je le gagne";
+$phrase2 = strrev($phrase1);
 
-$Phrase1 = "Engage le jeu que je le gagne";
-$Phrase2 = strrev($phrase1);
-$string_reverse = str_split($phrase2);
-$palindrome = "";
-foreach($string_reverse as $Value){
-    $palindrome .= $Value;
-}
+if($phrase2 = $phrase1){
+$Resultat = "palindrome";
+} 
+
+echo "La phrase $phrase1 est $Resultat";
