f725392abedefb660910cff1f974f83919308016
Hafida 3412 <lounsy81@gmail.com>
2024-02-24 10:42:59 +0100

    modifications

--- AlgoPHP/exo8.php
+++ AlgoPHP/exo8.php
@@ -2,20 +2,23 @@
 
 <p>Ecrire un algorithme qui renvoie la table de multiplication d'un nombre 
     passé en paramètre sous la forme:
+</p>
 
 <?php
+
 echo "<br>";
 
 echo "Table de 8:";
 
 echo "<br>";
 
-$i = 0;
+$i = 1;
 while($i <= 10){
-    echo $i * 8;
-    $i++;
     echo "<br>";
     echo "$i x 8 = ";
+
+    echo $i * 8;
+    $i++;
 }
 
 echo "<br>";
