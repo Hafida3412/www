88f37780c71dca3b93648aad680be2961a76c6db
Hafida 3412 <lounsy81@gmail.com>
2024-02-25 12:40:39 +0100

    exo15.php

---
+++ AlgoPHP/exo15.php
@@ -0,0 +1,33 @@
+<h1>Exercice 15</h1>
+
+<p>Créer une classe Personne (nom, prénom et date de naissance).
+    Instancier 2 personnes et afficher leurs informations: nom, prénom et âge.
+    <br>
+    $p1 = new Personne("DUPONT", "Michel", "1980-02-19");<br>
+    $p2 = new Personne("DUCHEMIN", "Alice", "1985-01-17");
+</p>
+
+<?php
+
+/*$p1 = new Personne("DUPONT", "Michel", "1980-02-19");
+$p2 = new Personne("DUCHEMIN", "Alice", "1985-01-17");*/
+
+    $newPersonne = [
+        "p1" => [
+            "prénom" => "Michel",
+            "nom" => "DUPONT",
+            "date de naissance" => "1980-02-19"
+        ],
+        "p2" => [
+            "prénom" => "Alice",
+            "nom" => "DUCHEMIN",
+            "date de naissance"=> "1985-01-17"
+        ]
+        ];
+    
+    foreach($newPersonne as $prénom => $nom) {
+        echo ucfirst($Prénom) . mb_strtoupper($Nom);
+    }
+
+
+
